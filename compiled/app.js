f6bff4b03b14183833e96b2ee7ec0b9483ac1437
niyobertin <niyonkurubbertin@gmail.com>
2024-04-01 23:28:04 +0200

    cors

--- compiled/app.js
+++ compiled/app.js
@@ -16,10 +16,14 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://niyobertin.github.io');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
+});
+// Handle preflight OPTIONS requests for PATCH
+app.options('/*', (req, res) => {
+    res.sendStatus(200); // Respond with 200 OK status
 });
 app.use("/api/v1", index_1.default);
 if (require.main === module) {
