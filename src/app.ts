7e6469e871b1ff9fb748278ec7c8af5209c65488
niyobertin <niyonkurubbertin@gmail.com>
2024-04-02 11:24:00 +0200

    cors

--- src/app.ts
+++ src/app.ts
@@ -10,14 +10,6 @@ db
     const app = express();
     app.use(express.json());
     app.use(cookieParser());
-    app.use(cors());
-    app.use((req: Request, res: Response, next: NextFunction) => {
-      res.setHeader('Access-Control-Allow-Origin', 'https://niyobertin.github.io'); 
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE,OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
-      next();
-  });
     app.use("/api/v1",routes);
     if (require.main === module) {
         const port = process.env.PORT || 3000;
